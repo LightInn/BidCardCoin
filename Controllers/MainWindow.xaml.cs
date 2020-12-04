969d5b8a51d6f42478736cf62569ddabbb72260b
bréval <breval2000@live.fr>
2020-12-04 16:17:12 +0100

    Image and Background

--- Controllers/MainWindow.xaml.cs
+++ Controllers/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
